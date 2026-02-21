47700d494d299046093bc72c726fe534d1e4d119
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-02-22 01:39:39 +0530

    added files sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,5 +1,6 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
@@ -16,5 +17,9 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  integrations:[Sentry.vercelAIIntegration],
+  integrations: [
+    Sentry.vercelAIIntegration,
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
 });
