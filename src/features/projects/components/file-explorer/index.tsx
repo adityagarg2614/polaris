1ad2d13beb551436043aa75a63adf71b1b98ba28
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-02-27 19:04:51 +0530

    changes

--- src/features/projects/components/file-explorer/index.tsx
+++ src/features/projects/components/file-explorer/index.tsx
@@ -13,11 +13,8 @@ import { CreateInput } from "./create-input"
 import { LoadingRow } from "./loading-row"
 import { Tree } from "./tree"
 
-export const FileExplorer = ({ 
-  projectId
-}: { 
-  projectId: Id<"projects">
-}) => {
+export const FileExplorer = ({ projectId }: { projectId: Id<"projects"> }) => {
+
   const [isOpen, setIsOpen] = useState(true);
   const [collapseKey, setCollapseKey] = useState(0);
   const [creating, setCreating] = useState<"file" | "folder" | null>(null);
@@ -32,7 +29,9 @@ export const FileExplorer = ({
 
 
   const handleCreate = (name: string) => {
-    setCreating(null);
+    console.log(creating)
+    setCreating(null);  //Doubt 
+    console.log(creating)
     if (creating === "file") {
       createFile({
         projectId,
